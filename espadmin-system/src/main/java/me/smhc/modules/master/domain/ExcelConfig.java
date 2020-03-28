910d75ea9cb44bc05f6922629f80bf2ae9122dcc
kyohaifeng <smhc$2019>
2020-03-28 15:40:44 +0900

    add import service and modify excelconfig
    add import service and modify excelconfig

--- espadmin-system/src/main/java/me/smhc/modules/master/domain/ExcelConfig.java
+++ espadmin-system/src/main/java/me/smhc/modules/master/domain/ExcelConfig.java
@@ -1,16 +1,15 @@
 package me.smhc.modules.master.domain;
 
-import lombok.Data;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
-import javax.persistence.CascadeType;
-import javax.validation.constraints.*;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import org.hibernate.annotations.*;
-import java.sql.Timestamp;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 /**
 * @author
