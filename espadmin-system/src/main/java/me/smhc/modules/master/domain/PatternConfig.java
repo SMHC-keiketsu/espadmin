38875fe6e61d79616c06895b634a1f41aac1a3c8
Mybuhe <47117726+Mybuhe@users.noreply.github.com>
2020-04-09 12:33:38 +0800

    master 下的 pattern更改dto 和 domain

--- espadmin-system/src/main/java/me/smhc/modules/master/domain/PatternConfig.java
+++ espadmin-system/src/main/java/me/smhc/modules/master/domain/PatternConfig.java
@@ -13,42 +13,26 @@ import java.io.Serializable;
 
 /**
 * @author 布和
-* @date 2020-04-08
+* @date 2020-04-09
 */
 @Entity
 @Data
 @Table(name="pattern_config")
 public class PatternConfig implements Serializable {
 
+    /** 搬入保税蔵置場 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull(groups = Tariff.Update.class)
+    @NotNull(groups = Update.class)
     @Column(name = "id")
     private Long id;
 
-    /** 代理店ID */
     @Column(name = "dept_id")
     private Long deptId;
 
     /** 同一部门内，名字不可重复 */
     @Column(name = "name")
     private String name;
-
-    /** 共同配置 */
-    @Column(name = "ida_mic_com_config")
-    private String idaMicComConfig;
-
-    /** IDA配置 */
-    @Column(name = "ida_config")
-    private String idaConfig;
-
-    /** MIC配置 */
-    @Column(name = "mic_config")
-    private String micConfig;
-
-    /** HCH配置 */
-    @Column(name = "hch_config")
-    private String hchConfig;
 
     @Column(name = "create_time")
     @CreationTimestamp
@@ -63,6 +47,136 @@ public class PatternConfig implements Serializable {
 
     @Column(name = "update_user_id")
     private Long updateUserId;
+
+    /** ＭＡＷＢ番号 */
+    @Column(name = "mawb_no",nullable = false)
+    @NotBlank
+    private String mawbNo;
+
+    /** 申告条件 */
+    @Column(name = "jyo",nullable = false)
+    @NotBlank
+    private String jyo;
+
+    /** 申告先種別コード */
+    @Column(name = "ic1",nullable = false)
+    @NotBlank
+    private String ic1;
+
+    /** 申告等種別コード */
+    @Column(name = "icb",nullable = false)
+    @NotBlank
+    private String icb;
+
+    /** 大額・少額識別 */
+    @Column(name = "ls",nullable = false)
+    @NotBlank
+    private String ls;
+
+    /** あて先官署コード */
+    @Column(name = "ch",nullable = false)
+    @NotBlank
+    private String ch;
+
+    /** あて先部門コード */
+    @Column(name = "chb",nullable = false)
+    @NotBlank
+    private String chb;
+
+    /** 申告予定年月日 */
+    @Column(name = "icd",nullable = false)
+    @NotBlank
+    private String icd;
+
+    /** 積載機名 */
+    @Column(name = "vsn",nullable = false)
+    @NotBlank
+    private String vsn;
+
+    /** 入港年月日 */
+    @Column(name = "arr",nullable = false)
+    @NotBlank
+    private String arr;
+
+    /** 取卸港コード */
+    @Column(name = "dst",nullable = false)
+    @NotBlank
+    private String dst;
+
+    /** 積出地コード */
+    @Column(name = "psc",nullable = false)
+    @NotBlank
+    private String psc;
+
+    /** 通関予定蔵置場コード */
+    @Column(name = "st",nullable = false)
+    @NotBlank
+    private String st;
+
+    /** 課税価格 */
+    @Column(name = "dpr",nullable = false)
+    @NotBlank
+    private String dpr;
+
+    /** 記事 */
+    @Column(name = "nt1",nullable = false)
+    @NotBlank
+    private String nt1;
+
+    /** 荷主セクションコード */
+    @Column(name = "nsc",nullable = false)
+    @NotBlank
+    private String nsc;
+
+    /** 荷主リファレンスナンバー */
+    @Column(name = "nrn",nullable = false)
+    @NotBlank
+    private String nrn;
+
+    /** 孫混載表示 */
+    @Column(name = "mkh",nullable = false)
+    @NotBlank
+    private String mkh;
+
+    /** 到着便名１ */
+    @Column(name = "fl1",nullable = false)
+    @NotBlank
+    private String fl1;
+
+    /** 到着便名２ */
+    @Column(name = "fl2",nullable = false)
+    @NotBlank
+    private String fl2;
+
+    /** 到着空港 */
+    @Column(name = "pot",nullable = false)
+    @NotBlank
+    private String pot;
+
+    /** 仕出地 */
+    @Column(name = "org",nullable = false)
+    @NotBlank
+    private String org;
+
+    /** ジョイント混載 */
+    @Column(name = "jnt",nullable = false)
+    @NotBlank
+    private String jnt;
+
+    /** 特殊貨物記号 */
+    @Column(name = "spc",nullable = false)
+    @NotBlank
+    private String spc;
+
+    /** 搬入保税蔵置場 */
+    @Column(name = "ihw",nullable = false)
+    @NotBlank
+    private String ihw;
+
+    /** 仕向地 */
+    @Column(name = "hch_dst",nullable = false)
+    @NotBlank
+    private String hchDst;
 
     public @interface Update {}
 
--- espadmin-system/src/main/java/me/smhc/modules/master/service/dto/PatternConfigDto.java
+++ espadmin-system/src/main/java/me/smhc/modules/master/service/dto/PatternConfigDto.java
@@ -6,28 +6,18 @@ import java.io.Serializable;
 
 /**
 * @author 布和
-* @date 2020-04-08
+* @date 2020-04-09
 */
 @Data
 public class PatternConfigDto implements Serializable {
 
+    /** 搬入保税蔵置場 */
     private Long id;
 
     private Long deptId;
 
     /** 同一部门内，名字不可重复 */
     private String name;
-    /** 共同配置 */
-    private String idaMicComConfig;
-
-    /** IDA配置 */
-    private String idaConfig;
-
-    /** MIC配置 */
-    private String micConfig;
-
-    /** HCH配置 */
-    private String hchConfig;
 
     private Timestamp createTime;
 
@@ -36,4 +26,82 @@ public class PatternConfigDto implements Serializable {
     private Timestamp updateTime;
 
     private Long updateUserId;
+
+    /** ＭＡＷＢ番号 */
+    private String mawbNo;
+
+    /** 申告条件 */
+    private String jyo;
+
+    /** 申告先種別コード */
+    private String ic1;
+
+    /** 申告等種別コード */
+    private String icb;
+
+    /** 大額・少額識別 */
+    private String ls;
+
+    /** あて先官署コード */
+    private String ch;
+
+    /** あて先部門コード */
+    private String chb;
+
+    /** 申告予定年月日 */
+    private String icd;
+
+    /** 積載機名 */
+    private String vsn;
+
+    /** 入港年月日 */
+    private String arr;
+
+    /** 取卸港コード */
+    private String dst;
+
+    /** 積出地コード */
+    private String psc;
+
+    /** 通関予定蔵置場コード */
+    private String st;
+
+    /** 課税価格 */
+    private String dpr;
+
+    /** 記事 */
+    private String nt1;
+
+    /** 荷主セクションコード */
+    private String nsc;
+
+    /** 荷主リファレンスナンバー */
+    private String nrn;
+
+    /** 孫混載表示 */
+    private String mkh;
+
+    /** 到着便名１ */
+    private String fl1;
+
+    /** 到着便名２ */
+    private String fl2;
+
+    /** 到着空港 */
+    private String pot;
+
+    /** 仕出地 */
+    private String org;
+
+    /** ジョイント混載 */
+    private String jnt;
+
+    /** 特殊貨物記号 */
+    private String spc;
+
+    /** 搬入保税蔵置場 */
+    private String ihw;
+
+    /** 仕向地 */
+    private String hchDst;
 }
